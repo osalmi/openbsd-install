fcfd1efe37fc0279d43e1dabb12a8dd15b97392b
Ossi Salmi <osalmi@iki.fi>
2011-11-17 14:52:09 +0200

    Added script for mirroring packages

---
+++ packages/fetch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ARCH="amd64 i386"
+LIST="../site/install.pkgs"
+SITE="http://ftp.eu.openbsd.org/pub/OpenBSD/5.0/packages"
+
+cd $(dirname $0)
+
+for a in $ARCH; do
+    test -d $a || mkdir $a
+    while read pkg _; do
+        ( cd $a && wget -N "${SITE}/${pkg}.tgz" )
+    done < $LIST
+done
