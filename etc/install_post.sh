869e09bdea5712978325266a83cbd88210b25824
Ossi Salmi <osalmi@iki.fi>
2012-05-08 14:28:18 +0300

    Create directories for preserved mounts

--- etc/install_post.sh
+++ etc/install_post.sh
@@ -9,4 +9,5 @@ if [ -s /tmp/fstab.preserve ]; then
     echo "Restoring preserved mounts:"
     cat /tmp/fstab.preserve
     cat /tmp/fstab.preserve >> /mnt/etc/fstab
+    while read dev dir _; do mkdir -p /mnt${dir}; done < /tmp/fstab.preserve
 fi
